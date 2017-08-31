e57e7bd173fea10bf9d114ace2049d9a1a3c38aa
Elan Kvitko <elan@idknyc.com>
2017-08-31 14:49:35 -0400

    refactor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-
   end
 
   def ocr_checker
@@ -8,15 +7,31 @@ class HomeController < ApplicationController
       @secondary = params[ :secondary ]
     end
 
-    uploaded_io = params[ :picture ]
+    link = get_link( params[ :picture ] )
+    image_url = convert_pdf_to_jpg( link )
+    ocr_completed = convert_jpg_to_ocr( image_url )
+
+    File.delete( Rails.root.join( params[ :picture ].original_filename ) )
+    session[ :ocr_text ] = ocr_completed
+    redirect_to controller: 'instructions', action: 'index', secondary: @secondary
+  end
+
+  def get_link( file )
+    uploaded_io = file
 
     file = File.open( Rails.root.join( uploaded_io.original_filename ), 'wb' ) do | file |
       file.write( uploaded_io.read )
     end
 
+    begin_pdf( uploaded_io )
+
+    result = upload_pic( "complete.pdf" )
+  end
+
+  def begin_pdf( file )
     pdf = CombinePDF.new
 
-    each_pdf = CombinePDF.load( uploaded_io.original_filename ).pages
+    each_pdf = CombinePDF.load( file.original_filename ).pages
 
     each_pdf.each_with_index do | page, idx |
       if idx == each_pdf.length - 1
@@ -25,30 +40,26 @@ class HomeController < ApplicationController
     end
 
     pdf.save "complete.pdf"
-    result = upload_pic( "complete.pdf" )
+  end
 
+  def convert_pdf_to_jpg( link )
     @process = cloud_convert.build_process(input_format: :pdf, output_format: :jpg)
     @process_response = @process.create
     @conversion_response = @process.convert(
           input: "download",
           outputformat: :jpg,
-          file: result,
+          file: link,
           download: "false"
         )
 
-    result_con = "http:" + @conversion_response[ :output ][ :url ]
+    result = "http:" + @conversion_response[ :output ][ :url ]
+  end
 
-    download = open( result_con )
+  def convert_jpg_to_ocr( link )
+    download = open( link )
     downloaded_file = IO.copy_stream( download, "image_convert.jpg" )
     file_url = upload_pic( "image_convert.jpg" )
-
-
     ocr_result = ocr_helper.convert url: file_url
-
-    ocr_text = ocr_result[ 0 ].to_a[ 2 ].join( " " ).downcase
-
-    File.delete( Rails.root.join( uploaded_io.original_filename ) )
-    session[ :ocr_text ] = ocr_text
-    redirect_to controller: 'instructions', action: 'index', secondary: @secondary
+    ocr_result[ 0 ].to_a[ 2 ].join( " " ).downcase
   end
 end
--- app/controllers/instructions_controller.rb
+++ app/controllers/instructions_controller.rb
@@ -1,42 +1,54 @@
 class InstructionsController < ApplicationController
   def index
     ocr_text = session[ :ocr_text ]
+    clean_session
+    @render = keyword_search( ocr_text )[ 0 ]
+  end
 
+  def clean_session
     session[ :ocr_text ] = nil
+  end
 
-    key_words = [ "allied", "oxford", "medicare", "healthfirst", "emblemhealth", "emblemhealth", "metroplus", "1199", "affinity", "blue" ]
+  def keyword_search( ocr_text )
+    text_found = []
+
+    key_words = [
+                  "allied",
+                  "oxford",
+                  "medicare",
+                  "healthfirst",
+                  "emblemhealth",
+                  "emblemhealth",
+                  "metroplus",
+                  "1199",
+                  "affinity",
+                  "blue"
+                ]
 
     key_hash = {
                 "allied" => "mail: allied benefit svstems, inc. \r\n",
                 "oxford" => "for members: \r\n" + "www.oxfordhealth.com",
                 "medicare" => "health care financing administration",
                 "healthfirst" => "healthfirst",
-                "emblemhealth" => {
-                                    "13551" => "ghi",
-                                    "2845" => "hip"
-                                  },
+                "emblemhealth" => { "13551" => "ghi", "2845" => "hip" },
                 "metroplus" => "metroplus",
                 "1199" => "1199seiu",
                 "affinity" => "affinity",
-                "blue" => {
-                            "blue cross" => "blue",
-                            "bluecross" => "blue"
-                          }
-              }
+                "blue" => { "blue cross" => "blue", "bluecross" => "blue" }
+               }
 
-    text_found = []
 
-    key_words.each do | key |
-      if ocr_text.include?( key )
-        if key_hash[ key ].is_a? Hash
-          key_hash[ key ].each do | identifier, payor |
-            text_found << payor if ocr_text.include? identifier
-          end
-        elsif ocr_text.include?( key_hash[ key ] )
-          text_found << key
-        end
-      end
-    end
-    @render = text_found[ 0 ]
+     key_words.each do | key |
+       if ocr_text.include?( key )
+         if key_hash[ key ].is_a? Hash
+           key_hash[ key ].each do | identifier, payor |
+             text_found << payor if ocr_text.include? identifier
+           end
+         elsif ocr_text.include?( key_hash[ key ] )
+           text_found << key
+         end
+       end
+     end
+    text_found
   end
 end
